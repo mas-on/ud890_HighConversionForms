200d8471bb4a2df22761649f9e0cbec87931c27a
M S home <mariaserg@gmail.com>
2019-05-12 18:56:41 +0300

    add old events hiding

--- final_project/src/js/events.js
+++ final_project/src/js/events.js
@@ -2,33 +2,10 @@ import _db from './lib/estorage.js';
 import _cmn from './lib/common.js';
 require("../css/events.css");
 
-
-
-function fillEvents(events) {
-    var errmsg = $("#err");
-
-    if (events.length === 0) {
-        errmsg.text("No events yet");
-        return;
-    }
-
-    //sort events by time
-    events.sort(function (a, b) {
-        return new Date(a["event-start"]) - new Date(b["event-start"]);
-    });
-
-    //group events to dictionary (key=date)
-    var dateEvents = events.reduce(function (acc, e) {
-        var key = new Date(e["event-start"]).toLocaleDateString();
-        if (!acc[key]) {
-          acc[key] = [];
-        }
-        acc[key].push(e);
-        return acc;
-      }, {});
-    
-    $.each(dateEvents, function (d, evts) {                
-        var dayItems = $("<ul></ul>").addClass("g-items");       
+//help functions
+function appendHtml(dateEvents) {
+    $.each(dateEvents, function (d, evts) {
+        var dayItems = $("<ul></ul>").addClass("g-items");
 
         $.each(evts, function (i, e) {
             //data
@@ -38,11 +15,11 @@ function fillEvents(events) {
 
             //caption elements
             var hid = $("<span></span>").addClass("hidden").text(id);
-            var time = _cmn.formatTime(estart.getHours(),estart.getMinutes());
+            var time = _cmn.formatTime(estart.getHours(), estart.getMinutes());
             var info = $("<span title='Click to see details'></span>")
                 .text(time + " " + name)
                 .addClass("clickable")
-                .click(function() {
+                .click(function () {
                     $(this).parent().siblings('.item-details').toggleClass("hidden");
                     return false;
                 });
@@ -63,7 +40,7 @@ function fillEvents(events) {
 
             //details elements
             var details = $("<div></div>").addClass("item-details hidden");
-            
+
             if (e["event-end"] !== undefined && e["event-end"].length > 0) {
                 var dtEnd = new Date(e["event-end"]);
                 var ld = dtEnd.toLocaleDateString();
@@ -80,34 +57,55 @@ function fillEvents(events) {
             var host = $("<div></div>").text("host: " + e["event-host"]);
             details.append(type, place, host);
 
-            if (e["event-msg"] !== undefined && e["event-msg"].length > 0) 
+            if (e["event-msg"] !== undefined && e["event-msg"].length > 0)
                 details.append($("<div></div>").text("info: " + e["event-msg"]));
-            
-            if (e["event-guests"] !== undefined && e["event-guests"].length > 0) 
+
+            if (e["event-guests"] !== undefined && e["event-guests"].length > 0)
                 details.append($("<div></div>").text("guests: " + e["event-guests"]));
 
             //parent elements            
             var item = $("<li></li>").addClass("event-item").append(caption, details);
+            if (estart < new Date())
+                item.addClass("past");
             dayItems.append(item);
         });
 
         var day = $("<span></span>").addClass("group-name").text(d);
-        var ditem = $("<li></li>").addClass("event-group").append(day,dayItems);
+        var ditem = $("<li></li>").addClass("event-group").append(day, dayItems);
+        if (dayItems.children().not(".past").length === 0)
+            ditem.addClass("hidden past");
+
         $("#event-list").append(ditem);
+        if ($("#event-list").children().not(".hidden").length === 0)
+            $("#show-past").removeClass("hidden");
     });
 }
 
-$(function () {
+function fillEvents(events) {
     var errmsg = $("#err");
-    if (!_db.supports_local_storage()) {
-        errmsg.text("Please, try another browser");
+
+    if (events.length === 0) {
+        errmsg.text("No events yet");
         return;
     }
 
-    var events = getEvents();
-    fillEvents(events);
+    //sort events by time
+    events.sort(function (a, b) {
+        return new Date(a["event-start"]) - new Date(b["event-start"]);
+    });
 
-});
+    //group events to dictionary (key=date)
+    var dateEvents = events.reduce(function (acc, e) {
+        var key = new Date(e["event-start"]).toLocaleDateString();
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(e);
+        return acc;
+    }, {});
+
+    appendHtml(dateEvents);
+}
 
 function getEvents() {
     return _db.get_from_storage('events');
@@ -117,4 +115,31 @@ function saveEvents(events) {
     _db.save_to_storage('events', events);
 }
 
+//on load
+$(function () {
+    var errmsg = $("#err");
+    if (!_db.supports_local_storage()) {
+        errmsg.text("Please, try another browser");
+        return;
+    }
+
+    $("#show-past").click(function(){
+        $(this).toggleClass("hidden");
+        $("#hide-past").removeClass("hidden");
+        $("#event-list").children().removeClass("hidden");
+    });
+
+    $("#hide-past").click(function(){
+        $(this).toggleClass("hidden");
+        $("#show-past").removeClass("hidden");
+        $.each($("#event-list .g-items"), function(i,el) {
+            if ($(el).children().not(".past").length === 0)
+                $(el).parent().addClass("hidden");
+        });
+        
+    });
+
+    var events = getEvents();
+    fillEvents(events);    
+});
 
